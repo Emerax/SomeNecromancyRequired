b650a82b278715c386dd92cc6d447f115b7860b1
Emil Segerbäck <emilsegerback@mykolab.ch>
2020-05-09 22:12:29 +0200

    Make particle systems stay alive until all particles are gone

--- game/scenes/projectile.gd
+++ game/scenes/projectile.gd
@@ -1,12 +1,14 @@
 extends Spatial
 
-
-var target;
+var target
 var target_position = Vector3(0, 0, 0)
-var initial_velocity: Vector3;
-var speed = 15;
-var initial_velocity_contribution = 1;
-var damage = 1;
+var initial_velocity: Vector3
+var speed = 15
+var initial_velocity_contribution = 1
+var damage = 1
+var hit_target = false
+var particles: Particles = null
+var remove_timer = Timer.new()
 
 
 # warning-ignore:shadowed_variable
@@ -18,15 +20,28 @@ func init(target, position, initial_velocity, dmg):
 	self.initial_velocity = initial_velocity
 	self.damage = dmg
 
+	self.particles = get_node(@"Particles")
+	self.remove_timer.wait_time = self.particles.lifetime
+	self.remove_timer.connect("timeout", self, "_on_Timer_timeout")
+
+
+func _on_Timer_timeout():
+	queue_free()
+
 
 func _process(delta):
 	var pos_diff = target_position - self.get_global_transform().origin
 
 	# Delete self when at target
 	if pos_diff.length_squared() < 0.1:
-		queue_free()
-		if target.health > 0:
-			target.take_damage(damage)
+		particles.emitting = false
+
+		if not hit_target:
+			hit_target = true
+			if target.health > 0:
+				target.take_damage(damage)
+			remove_timer.start()
+
 		return
 
 	# Accelerate towards the target
