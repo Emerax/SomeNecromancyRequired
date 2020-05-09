bd81abe4942888fed1bea7e56d64d8aeab082942
TheZoq2 <frans.skarman@protonmail.com>
2020-05-09 13:59:12 +0200

    make enemies stop if something is blocking them

--- game/scenes/EnemySpawner.gd
+++ game/scenes/EnemySpawner.gd
@@ -5,6 +5,7 @@ var template = preload("enemy.tscn")
 
 var grid;
 
+var last_spawn = null
 var since_last_spawn = 0
 
 
@@ -20,8 +21,9 @@ func _process(delta):
 	if self.since_last_spawn > 3:
 		var new_enemy = template.instance()
 		print(self.lane)
-		new_enemy.init(self.transform, self.lane, self.grid)
+		new_enemy.init(self.transform, self.lane, self.grid, self.last_spawn)
 		# add_child(new_enemy)
 		# new_enemy.set_owner(get_tree().get_root())
 		get_tree().get_root().call_deferred("add_child", new_enemy)
 		self.since_last_spawn = 0
+		self.last_spawn = new_enemy
--- game/scenes/enemy.gd
+++ game/scenes/enemy.gd
@@ -8,15 +8,17 @@ extends Spatial
 var lane = 0
 var speed = 3
 var since_last_attack = 0
+var next_in_line = null
 
 var grid
 
 var projectile_template = preload("Fireball.tscn")
 
-func init(transform, lane, grid):
+func init(transform, lane, grid, next_in_line):
 	self.lane = lane
 	self.grid = grid
 	self.transform = transform
+	self.next_in_line = next_in_line
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,15 +27,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.transform = self.transform.translated(Vector3(0, 0, -self.speed) * delta)
 
 	since_last_attack += delta
 
+	var can_move: bool = true
 	# This is only ranged attacks
 	# Check if there is something to attack and if the cooldown is over
 	var target = self.grid.get_first_defender(self.lane)
-	if target != null and since_last_attack > 1:
-		since_last_attack = 0;
-		var projectile = projectile_template.instance()
-		projectile.init(target, self.get_global_transform(), Vector3(0, 3, 0))
-		get_tree().get_root().add_child(projectile)
+	if target != null:
+		if since_last_attack > 1:
+			since_last_attack = 0;
+			var projectile = projectile_template.instance()
+			projectile.init(target, self.get_global_transform(), Vector3(0, 3, 0))
+			get_tree().get_root().add_child(projectile)
+
+		var d = target.get_global_transform().origin - get_global_transform().origin;
+		if d.length() < 4:
+			can_move = false
+
+	# Check if we ran into a ghoul or someone ahead of us
+	if self.next_in_line != null:
+		var d = next_in_line.get_global_transform().origin - get_global_transform().origin;
+		if d.length() < 4:
+			can_move = false
+
+	if can_move:
+		self.transform = self.transform.translated(Vector3(0, 0, -self.speed) * delta)
+
